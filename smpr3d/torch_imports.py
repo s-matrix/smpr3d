07dff0694cbb4da6f1428c5f45eae83bf3854058
Philipp Pelz <philipp.pelz@berkeley.edu>
2021-03-29 10:35:17 -0700

    first commit

---
+++ smpr3d/torch_imports.py
@@ -0,0 +1,5 @@
+import torch as th
+from torch import as_tensor,Tensor,ByteTensor,LongTensor,FloatTensor,HalfTensor,DoubleTensor
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.distributed as dist
