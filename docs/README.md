bd37cb0c465abe3542d9b7e6fb99ab9f095417a2
Philipp Pelz <philipp.pelz@berkeley.edu>
2021-03-30 09:55:28 -0700

    added slurm

---
+++ docs/README.md
@@ -0,0 +1,4 @@
+## smpr3d docs
+
+This is the source of the documentation for smpr3d. It is generated with nbdev from the smpr3d repo. Don't edit things here directly.
+ 
---
+++ examples/slurm.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=smatrix2    # Job name
+#SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
+
+#SBATCH -C gpu
+#SBATCH -N 1
+#SBATCH -t 5
+#SBATCH -c 10
+#SBATCH --gres=gpu:1
+#SBATCH -A m1759
+#SBATCH --ntasks-per-node=1
+#SBATCH -q special
+#SBATCH --output=serial_test2.log
+pwd; hostname; date
+
+module load cgpu
+module load pytorch/1.8.0-gpu
+#rm serial_test2.log
+srun -N 1 python ~/smpr3d/examples/reconstruct_simul_data.py
+
+date
