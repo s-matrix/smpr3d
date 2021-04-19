a147679c09db77bb899b50ac31513cb1ab08cb00
Philipp Pelz <philipp.pelz@berkeley.edu>
2021-04-19 13:10:31 -0700

    multi gpu single node

--- examples/reconstruct_simul_data_multi_gpu_single_node.py
+++ examples/reconstruct_simul_data_multi_gpu_single_node.py
@@ -25,7 +25,7 @@ args.io.filename_data = 'atoms_aberrations_big.h5'
 
 summary = setup_logging(args.io.path, args.io.logname)
 
-args.dist_backend = 'nccl'  # 'mpi'
+args.dist_backend = 'mpi'  # 'mpi'
 args.dist_init_method = f'file://{args.io.path}sharedfile'
 args.node_config = configure_node(args.dist_backend, args.dist_init_method)
 
